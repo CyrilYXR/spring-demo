5fde013856837d6c662a64dd0e808f91a28cdfa0
Cyril <cyril.yao@oocl.com>
2020-01-13 15:57:09 +0800

    initial

---
+++ src/test/java/com/yaocy/sping/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaocy.sping.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
